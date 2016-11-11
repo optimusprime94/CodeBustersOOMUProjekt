58993ce9cbaef0cc0a75a6bf7c847f73da7b41c0
Blaerwkfdr FSDgfderg <annoyancekeepshauntingme@hotmail.com>
2016-11-11 13:02:18 +0100

    new commit

---
+++ projectArena/src/arena/users/Advertiser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arena.users;
+
+/**
+ *
+ * @author S153298
+ */
+public class Advertiser extends User{
+    
+}
