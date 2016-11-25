7c902e1811fa6a6490356ecdfae4d31ef5a7f2e9
optimusprime94 <dzekson94@gmail.com>
2016-11-25 14:43:03 +0100

    gamestarter

---
+++ projectArena/src/controller/GameStarter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import gameinstaller.Game;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import view.ArenaView;
+
+/**
+ *
+ * @author S143522 (Elvir Dzeko)
+ */
+public class GameStarter {
+
+    private Game game;
+    
+    public GameStarter(Game game) {
+        this.game = game;
+        startGame();
+    }
+
+    private void startGame() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar", game.getGameName() + ".jar");
+            pb.directory(new File(game.getGamePath()));
+            Process p = pb.start();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ArenaView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
