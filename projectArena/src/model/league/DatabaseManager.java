f8bb913c8f85d9fd314383df6901ea56eba7d852
mastermind92 <felixceriksson@gmail.com>
2016-12-23 11:57:28 +0100

    create Tournament wiew

---
+++ projectArena/src/model/league/DatabaseManager.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.league;
+
+import java.sql.*;
+
+import java.util.ArrayList;
+import model.advertisement.Advertisement;
+
+
+/**
+ *
+ * @author S153977
+ */
+public class DatabaseManager 
+{
+    private ArrayList<String> gamesLibrary;
+    private ArrayList<String> tournaments;
+    private ArrayList<Advertisement> adverts;
+    
+    public DatabaseManager()
+    {
+        this.gamesLibrary = new ArrayList<>();
+        this.tournaments = new ArrayList<>(); 
+        this.adverts = new ArrayList<>(); 
+    }
+    
+    public ArrayList getGames()
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
+            Statement statement = connection.createStatement();
+            String message = ("select* from gameLibrary");
+            ResultSet resultSet = statement.executeQuery(message);
+
+            while (resultSet.next()) 
+            {
+                this.gamesLibrary.add(resultSet.getString(2));
+            } 
+        }
+        catch(Exception ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+        return(gamesLibrary);
+    }
+    public ArrayList getTournaments()
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
+            Statement statement = connection.createStatement();
+            String message= "select* from tournament";
+            ResultSet resultSet = statement.executeQuery(message);
+            while (resultSet.next())
+            {
+                String name = resultSet.getString(2);
+                int visibility = Integer.parseInt(resultSet.getString(3));
+                if (visibility == 1)
+                {
+                    this.tournaments.add(name);
+                }    
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+        return(tournaments);
+    }
+    public ArrayList getAdverts()
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
+            Statement statement = connection.createStatement();
+            String message= "select* from adverts";
+            ResultSet resultSet = statement.executeQuery(message);
+            while (resultSet.next())
+            {
+                String filePath = resultSet.getString(2);
+                String timeLenth = resultSet.getString(3);
+                String url = resultSet.getString(4);
+                Advertisement advert = new Advertisement(filePath, timeLenth, url);
+                adverts.add(advert); 
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+        return(adverts);
+    }
+}
---
+++ projectArena/src/model/league/OrganizeTournamentView.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.league;
+
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import model.administration.Game;
+import model.advertisement.Advertisement;
+import view.administration.InstallGameView;
+
+/**
+ *
+ * @author S153298
+ */
+public class OrganizeTournamentView {
+    
+    private BorderPane arenaFrame;
+    public OrganizeTournamentView(BorderPane arenaFrame)
+    {
+        this.arenaFrame = arenaFrame;
+    }
+    public void show()
+    {
+        Button btCreateTournament = new Button("Create tournament");
+        TextField tournamentName = new TextField();
+        TextField numberOfPlayers = new TextField();
+        TextField advertisment = new TextField();
+        Label labelNumberOfPlayers = new Label("Number of players");
+        Label labelTournamentName = new Label("Tournament name");
+        Label labelAdverts = new Label("Select Advertisment id (1-4)");
+       
+        ComboBox<String> boxTournaments = new ComboBox<>();
+        ComboBox<String> boxGames = new ComboBox<>();
+        boxGames.setPromptText("Select Game                ");
+        boxTournaments.setPromptText(("Select tournament"));
+        DatabaseManager dbtManager = new DatabaseManager();
+        ObservableList<String> tournaments = FXCollections.observableArrayList(dbtManager.getTournaments());
+        ObservableList<String> games = FXCollections.observableArrayList(dbtManager.getGames()); 
+        ArrayList<Advertisement> adverts = new ArrayList<>();
+   
+        boxGames.getItems().addAll(games);
+        boxTournaments.getItems().addAll(tournaments);
+        
+        
+        VBox vBox = new VBox();
+        arenaFrame.setCenter(vBox);
+        vBox.getChildren().addAll(labelTournamentName, tournamentName, labelAdverts, advertisment, labelNumberOfPlayers, numberOfPlayers, boxGames, boxTournaments, btCreateTournament);
+        vBox.setPadding(new Insets(20));
+        btCreateTournament.setOnAction(e->{
+            String tourName = tournamentName.getText();
+            int nrOfPlayers = Integer.parseInt(numberOfPlayers.getText());
+            String game = boxGames.getValue();
+            String tourStyle = boxTournaments.getValue();
+            int advertID = Integer.parseInt(advertisment.getText());
+            if (tourStyle.matches("Elemination tournament"))
+            {
+                if (game.matches("Othello"));
+                {
+                    Game othello = new Game("othello", "1", "src/gamelib", "image/icons/othello.png");
+                    Tournament newTournament = new EleminationTournament(nrOfPlayers, othello);
+                    newTournament.addAdvert(adverts.get(advertID-1));
+                    newTournament.conductTournament();
+                }
+            }
+            
+            System.out.println("You created a " + boxTournaments.getValue() + " tournament with " + numberOfPlayers.getText() + " players, named " + tournamentName.getText());
+            arenaFrame.getChildren().remove(vBox);
+        });
+        
+    }
+    
+}
