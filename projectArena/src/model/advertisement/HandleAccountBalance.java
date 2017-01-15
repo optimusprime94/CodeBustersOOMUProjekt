8044501c2f3b65aae52f271b66f8ca9ffaa6c185
mastermind92 <felixceriksson@gmail.com>
2017-01-15 13:23:43 +0100

    HandleAccoutBalance

--- projectArena/src/model/advertisement/HandleAccountBalance.java
+++ projectArena/src/model/advertisement/HandleAccountBalance.java
@@ -6,6 +6,7 @@
 package model.advertisement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.application.Application; 
@@ -17,35 +18,47 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.Scene; 
+import model.administration.Game;
 /**
  *
  * @author JohnBosco
  */
 public class HandleAccountBalance {
-    public static void addFund(int id){
-        try{
+    public static void addFund(int id, int money)
+    {
+        try
+        {
             Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
             Statement statement = connection.createStatement();
-            // String SQLMessage =String SQLMessage = "Increse money where the ID is =" +id;
-            //statement.executeUpdate(SQLMessage);
+            String SQLMessage = "update advertiser set Cash = Cash + "+ money+" where ID = "+id+";";
+            statement.executeUpdate(SQLMessage);
             connection.close();
         }
-        catch (SQLException ex){
+        catch (SQLException ex)
+        {
             System.out.print(ex.getMessage());
         } 
     }
-        public static int getBalance(int id){
-            
-            try{
-                Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
-                Statement statement = connection.createStatement();
-                connection.close();
+
+    public static int getBalance(int id) 
+    {
+        int balance = 0;
+        try 
+        {
+            Connection connection = DriverManager.getConnection("jdbc:sqlserver://hitsql-db.hb.se:56077;database=dbtht1629;user=dbtht1629;password=hiss99");
+            Statement statement = connection.createStatement();
+            String SQLMessage = "Select Cash from advertiser where id = " + id + "";
+            ResultSet resultSet = statement.executeQuery(SQLMessage);
+            while (resultSet.next()) 
+            {
+                balance = Integer.parseInt(resultSet.getString(1));
             }
-            catch (SQLException ex){
-                System.out.print(ex.getMessage());
-            }
-            finally{
-                return 50;
-            }
+            connection.close();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.print(ex.getMessage());
         }
+        return(balance);
+    }
 }
