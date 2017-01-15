cc8bf082e47210f3dc593433d9bad68287d807a0
DragonZlayer <s153298@student.hb.se>
2017-01-15 12:42:01 +0100

    tournamentStyle

--- projectArena/src/model/users/Operator.java
+++ projectArena/src/model/users/Operator.java
@@ -4,16 +4,13 @@
  * and open the template in the editor.
  */
 package model.users;
-import java.util.Scanner;
 
 /**
  *
  * @author S153298
  */
 public class Operator extends User{
-    Operator op = new Operator(); 
-    Scanner sc = new Scanner(System.in);
-    Advertiser advert;
+
      /**
      * This function will return the userType for this class.
      * @return 
@@ -24,16 +21,3 @@ public class Operator extends User{
     }    
 
 }
-/*public boolean replyRequest(){
-String answer;
-advert.sendRequest();
-System.out.println("Does the sadvertiser have your permission? Yes or No");
-answer = sc.nextLine();
-if (answer == "yes"){
-   return true;  
-}
-else 
-    return false; 
-}
-}
-*/
